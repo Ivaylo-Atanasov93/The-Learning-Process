5f0580bff84fc9d5a8d3e24aa9531c955b720d30
Ivaylo_Atanasov <ivailo.atanasov93@gmail.com>
2020-10-09 13:26:06 +0300

    Classes and Instances Lecture Tasks

---
+++ Classes_and_Instances-Lab/Glass.py
@@ -0,0 +1,29 @@
+class Glass:
+
+    capacity = 250
+
+    def __init__(self):
+        self.content = 0
+
+    def fill(self, ml:int):
+        if Glass.capacity < ml:
+            return f"Cannot add {ml} ml"
+        Glass.capacity -= ml
+        self.content += ml
+        return f"Glass filled with {ml} ml"
+
+    def empty(self):
+        Glass.capacity = 250
+        self.content = 0
+        return "Glass is now empty"
+
+    def info(self):
+        return f"{Glass.capacity} ml left"
+
+
+glass = Glass()
+print(glass.fill(100))
+print(glass.fill(200))
+print(glass.empty())
+print(glass.fill(200))
+print(glass.info())
