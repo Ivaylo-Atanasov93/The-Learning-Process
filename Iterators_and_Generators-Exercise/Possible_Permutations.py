792be92a6c1f584110e51e9adfa722a31a405361
Ivaylo_Atanasov <ivailo.atanasov93@gmail.com>
2020-10-19 14:42:30 +0300

    Task Update

--- Iterators_and_Generators-Exercise/Possible_Permutations.py
+++ Iterators_and_Generators-Exercise/Possible_Permutations.py
@@ -1,9 +1,9 @@
 import itertools
 
 def possible_permutations(iter):
-    result = list(itertools.permutations(iter))
-    result = [list(item) for item in result]
-    yield result
+    for permutation in itertools.permutations(iter):
+        yield list(permutation)
+
 
 
 [print(n) for n in possible_permutations([1, 2, 3])]
