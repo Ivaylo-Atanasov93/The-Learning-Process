ac0b711052b0ee296dae5b6a913db4d4db377593
Ivaylo Atanasov <ivailo.atanasov93@gmail.com>
2021-02-12 18:06:22 +0000

    Fixing the forms.

--- PythonWebBasics_Django/Django_Web_Basics/todos_app/forms.py
+++ PythonWebBasics_Django/Django_Web_Basics/todos_app/forms.py
@@ -1,43 +1,53 @@
 from django import forms
+from django.core.validators import ValidationError
 
-
-# class TodoForm(forms.Form):
-#     title = forms.CharField(
-#         max_length=30,
-#         widget=forms.TextInput(
-#             attrs={
-#                 'class': 'neshto-si'
-#             }
-#         )
-#     )
-#     description = forms.CharField()
-#
-#     def __init__(self):
-#         super().__init__()
-#         for (field_name, field) in self.fields.items():
-#             if 'class' in field.widget.attrs:
-#                 value = field.widget.attrs['class'] + ' form-control'
-#             else:
-#                 value = 'form-control'
-#             field.widget.attrs['class'] = value
 
 class TodoForm(forms.Form):
+
     title = forms.CharField(
+        min_length=5,
         max_length=30,
         widget=forms.TextInput(
             attrs={
                 'class': 'form_control',
             }
-        )
+        ),
+        required=True,
     )
     description = forms.CharField(
+        min_length=10,
         widget=forms.Textarea(
             attrs={
                 'class': 'form_control',
             }
         ),
-        required=False,
+        required=True,
     )
+
+    def clean_bot_catcher(self):
+        if len(self.cleaned_data['bot_catcher']):
+            raise ValidationError('This is a bot!')
+
+
+def min_validator_title(value):
+    if not value or len(value) < 5:
+        raise ValidationError(f'Value should be at least 5 symbols, it`s {len(value)} instead!')
+
+
+def min_validator_description(value):
+    if not value or len(value) < 10:
+        raise ValidationError(f'Value should be at least 10 symbols, it`s {len(value)} instead!')
+
+
+def max_validator_title(value):
+    if len(value) > 30:
+        raise ValidationError(f'Value should be maximum 30 symbols, it`s {len(value)} instead')
+
+
+def max_validator_description(value):
+    if len(value) > 100:
+        raise ValidationError(f'Value should be maximum 100 symbols, it`s {len(value)} instead')
+
     # password = forms.CharField(
     #     widget=forms.PasswordInput(
     #         attrs={
